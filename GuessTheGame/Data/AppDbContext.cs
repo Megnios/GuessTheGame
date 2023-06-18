378257b53e89e4305167dbeddba0f4b15094e3e3
Megnios <nbalciunas@fi.uba.ar>
2023-06-18 00:12:06 -0300

    Subo la preview

---
+++ GuessTheGame/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GuessTheGame.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuessTheGame.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+
+        }
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ GuessTheGame/Models/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace GuessTheGame.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Launch Year")]
+        public string LaunchYear { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string Company { get; set; }
+    }
+}
